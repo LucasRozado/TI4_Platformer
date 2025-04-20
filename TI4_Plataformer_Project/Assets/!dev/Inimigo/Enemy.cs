6a0f94650a40dd964b88470a3a5da86569b8d84d
Lucas Bento Rozado <lucasbrozado@gmail.com>
2025-04-20 09:57:27 -0300

    inicio navmesh
    Simples implementação de perseguição  para testes com navmesh

---
+++ TI4_Plataformer_Project/Assets/!dev/Inimigo/Enemy.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+
+    private NavMeshAgent agent;
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        agent.SetDestination(target.position);
+    }
+}
