76059192af01b0b628534c28406a5634faa9ccd6
Gabriel Meyer <gmeyerp@gmail.com>
2025-04-29 16:21:24 -0300

    plantas via animacao funcionando

--- TI4_Plataformer_Project/Assets/!dev/Jungle Interativos/DamagingPlatform.cs
+++ TI4_Plataformer_Project/Assets/!dev/Jungle Interativos/DamagingPlatform.cs
@@ -4,15 +4,22 @@ public class DamagingPlatform : MonoBehaviour
 {
     [SerializeField] float radius;
     [SerializeField] Transform sphereOrigin;
+    [SerializeField] Collider platformCollider;
     public void DealDamage()
     {
+        platformCollider.enabled = false;
         Collider[] hit = Physics.OverlapSphere(sphereOrigin.position, radius);
         foreach (Collider col in hit)
         {
-            if (TryGetComponent<Player>(out Player player))
+            if (col.gameObject.TryGetComponent<Player>(out Player player))
             {
                 Debug.Log("Player hit");
             }
         }
+    }
+
+    public void EnablePlatform()
+    {
+        platformCollider.enabled = true;
     }
 }
