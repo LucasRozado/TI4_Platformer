d4066b6c3d3f5158226ca58519861f7487b8cac2
Lucas <144150227+ChicoCirrose@users.noreply.github.com>
2025-06-29 20:16:45 -0300

    Updates

--- TI4_Plataformer_Project/Assets/Scripts/Levels/PowerUpTrigger.cs
+++ TI4_Plataformer_Project/Assets/Scripts/Levels/PowerUpTrigger.cs
@@ -10,6 +10,7 @@ public class PowerUpTrigger : MonoBehaviour
         {
             GameManager.powerUp.AcquirePowerUp(powerUp);
             Destroy(gameObject, 2f); // Destroy the power-up after 2 seconds
+            UIManager.instance.ShowText("You have gained the nimbleness of the Farus Monster. Jump to CLIMB", 15f);
         }
     }
 }
