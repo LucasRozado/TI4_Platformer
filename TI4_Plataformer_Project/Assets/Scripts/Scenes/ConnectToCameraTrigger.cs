6008e96bc184b2fee98dcef61ad8c6d85f4a36b0
Lucas <144150227+ChicoCirrose@users.noreply.github.com>
2025-07-07 18:33:04 -0300

    HotFix+Build 1.0.1

--- TI4_Plataformer_Project/Assets/Scripts/Scenes/ConnectToCameraTrigger.cs
+++ TI4_Plataformer_Project/Assets/Scripts/Scenes/ConnectToCameraTrigger.cs
@@ -7,10 +7,18 @@ using Unity.Cinemachine;
 public class ConnectToCameraTrigger : MonoBehaviour
 {
     [SerializeField] private CameraSwitch cameraTrigger;
+    [SerializeField] private bool lockIntoPlayer = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         cameraTrigger.SetTargetCamera(this.GetComponent<CinemachineCamera>());
-
+        if (lockIntoPlayer)
+        {
+            LockCameraToPlayer();
+        }
+    }
+    private void LockCameraToPlayer()
+    {
+        this.gameObject.GetComponent<CinemachineCamera>().Follow = Player.instance.transform;
     }
 }
